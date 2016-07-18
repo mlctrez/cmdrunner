245d1a8b56b4599b8b510ee0c9a999e408a03c9f
mlctrez <mlctrez@gmail.com>
2016-07-17 20:49:39 -0500

    add docs

--- cmdrunner.go
+++ cmdrunner.go
@@ -8,20 +8,27 @@ import (
 	"syscall"
 )
 
+// CmdChannel represents the constant type of stdout or stderr.
 type CmdChannel int
 
 const (
+	// CmdStdout is the constant representing stdout.
 	CmdStdout CmdChannel = iota
+	// CmdStderr is the constant representing stderr.
 	CmdStderr
 )
 
+// OutputSink represents a function that will receive CmdOutput structs.
 type OutputSink func(out *CmdOutput)
 
+// CmdOutput is the combination of channel and text output.
+// Empty lines from the output are not suppressed by this framework.
 type CmdOutput struct {
 	Channel CmdChannel ``
 	Text    string
 }
 
+// CmdRunner wraps exec.Cmd to provide additional functionality.
 type CmdRunner struct {
 	cmd    *exec.Cmd
 	output chan *CmdOutput
@@ -29,6 +36,7 @@ type CmdRunner struct {
 	sink   OutputSink
 }
 
+// NewCmdRunner creates a new CmdRunner, wrapping the provided exec.Cmd.
 func NewCmdRunner(cmd *exec.Cmd) *CmdRunner {
 	ch := make(chan *CmdOutput, 1)
 	return &CmdRunner{cmd: cmd, output: ch}
@@ -54,6 +62,7 @@ func (r *CmdRunner) readPipe(readerFunc func() (io.ReadCloser, error), ch CmdCha
 	return nil
 }
 
+// Start creates the stdout and stderr readers and sends output to the provided OutputSink.
 func (r *CmdRunner) Start(outputSink OutputSink) error {
 	r.sink = outputSink
 
@@ -74,6 +83,7 @@ func (r *CmdRunner) Start(outputSink OutputSink) error {
 
 }
 
+// WaitExit waits for the command to exit, returning the exit code.
 func (r *CmdRunner) WaitExit() int {
 
 	r.wg.Wait()
