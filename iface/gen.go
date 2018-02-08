0614ce347b78bff8f8f689da47e99a3b5d8c52fc
Matt Crawford <mlctrez@gmail.com>
2018-02-08 16:58:38 -0600

    correct package name

--- iface/gen.go
+++ iface/gen.go
@@ -2,4 +2,4 @@ package cmdrunner
 
 // generate an interface for CmdRunner
 
-//go:generate ifacemaker -f ../cmdrunner.go -s CmdRunner -p cmdrunner -i CmdRunnerImpl -o iface.go -r cmdrunner -a github.com/mlctrez/cmdrunner
+//go:generate ifacemaker -f ../cmdrunner.go -s CmdRunner -p iface -i CmdRunnerImpl -o iface.go -r cmdrunner -a github.com/mlctrez/cmdrunner
